6f8f0c818b38c376818d42d388fcca83f687ecea
TemeK1 <50360933+TemeK1@users.noreply.github.com>
2019-06-13 12:36:07 +0300

    Update dynamic_content.js

--- dynamic_content.js
+++ dynamic_content.js
@@ -1,18 +1,18 @@
-function w3IncludeHTML() {
+function IncludeHTML() {
   var z, i, a, file, xhttp;
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
-    if (z[i].getAttribute("w3-include-html")) {
+    if (z[i].getAttribute("include-html")) {
       a = z[i].cloneNode(false);
-      file = z[i].getAttribute("w3-include-html");
+      file = z[i].getAttribute("include-html");
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-          a.removeAttribute("w3-include-html");
+          a.removeAttribute("include-html");
           a.innerHTML = xhttp.responseText;
 
           z[i].parentNode.replaceChild(a, z[i]);
-          w3IncludeHTML();
+          IncludeHTML();
         }
       }      
       xhttp.open("GET", file, true);
