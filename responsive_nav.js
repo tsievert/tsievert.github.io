e2909053c058f9cb8fe5343335011a985f162e2c
Thorbjörn Sievert <31933140+tsievert@users.noreply.github.com>
2023-08-25 21:59:41 +0200

    Revert "navbar"
    This reverts commit 3aea8b8264bc318bb8f67c7f07d0142cb686ac20.

--- responsive_nav.js
+++ responsive_nav.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+f/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsiveNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
