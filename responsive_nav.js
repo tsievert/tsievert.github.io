1f2e1a1d9ed1651d8f7b66d532639805b027b47e
Thorbjörn Sievert <31933140+tsievert@users.noreply.github.com>
2023-08-25 23:28:26 +0200

    navbar

--- responsive_nav.js
+++ responsive_nav.js
@@ -3,6 +3,12 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburgerMenu.addEventListener('click', () => {
   navLinks.classList.toggle('show');
+
+  if (navLinks.classList.contains('show')) {
+    hamburgerMenu.classList.add('expanded');
+  } else {
+    hamburgerMenu.classList.remove('expanded');
+  }
 });
 
 navLinks.querySelectorAll('.nav-item').forEach(link => {
