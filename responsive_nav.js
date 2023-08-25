3aea8b8264bc318bb8f67c7f07d0142cb686ac20
Thorbjörn Sievert <31933140+tsievert@users.noreply.github.com>
2023-08-25 21:39:24 +0200

    navbar

--- responsive_nav.js
+++ responsive_nav.js
@@ -1,4 +1,4 @@
-f/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsiveNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
