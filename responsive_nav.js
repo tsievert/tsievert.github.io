d02bd195fc05bdf865502e3b78978a503b88e3e5
Thorbjörn Sievert <31933140+tsievert@users.noreply.github.com>
2023-08-25 23:36:31 +0200

    revert

--- responsive_nav.js
+++ responsive_nav.js
@@ -1,23 +1,8 @@
-const navbar = document.querySelector('.navbar');
-const toggleButton = document.querySelector('.toggle-button');
-
-const toggleNavbar = () => {
-  navbar.classList.toggle('active');
-};
-
-toggleButton.addEventListener('click', toggleNavbar);
-
-const navLinks = document.querySelectorAll('.navbar a');
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    navbar.classList.remove('active');
-  });
-});
-
-const currentPath = window.location.pathname;
-
-navLinks.forEach(link => {
-  if (link.getAttribute('href') === currentPath) {
-    link.classList.add('active');
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-});
+}
